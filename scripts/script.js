d3c325f6831b3a7295bfd2d46608d3a91becae2a
Karakol-KG <rustamabdyrahmanov10@gmail.com>
2022-11-20 00:31:12 +0600

    tipy peremennyh

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+const num = 7777
+console.log( num )
+
+const a = 10
+const b = 2
+const c = 5
+console.log( a + b + c )
+
+const h = 100
+console.log( h *2 )
+
+const r = 67
+console.log(r % 7)
+
+const a1= 10
+const b1= 2
+console.log( a1 + b1)
+console.log( a1 - b1)
+console.log( a1 * b1)
+console.log( a1 / b1)
+
+const c1 = 15
+const d =2
+console.log( c1 + d)
+const result = 12
+console.log( result)
+
+
+const a2 =30
+const b2 =15
+console.log(a2 +b2)
+const c2 = 45
+console.log(c2 % 4)
+
+
+const a3 = 17
+const b3 = 10
+console.log( a3 -b3)
+const c3 = 7
+const d3 =100
+console.log( c3 +d3 )
+const result1=107
+console.log( result1 )
+
